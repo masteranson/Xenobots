8d58e94a029fd063730b24112a3f9c121b26eaca
Anson Tsang <ytsang3@gatech.edu>
2022-12-22 20:42:33 +0800

    multiple cubes

--- generate.py
+++ generate.py
@@ -1,7 +1,33 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+length = 1
+width = 1
+height = 1
+
+x= 0
+y = 0
+z = 0.5
+
+for i in range (0,3):
+
+    x = x + 1
+    y = 0
+    height = 1
+    for j in range (0, 3):
+
+        y = y + 1
+        z = 0.5
+        height = 1
+        for k in range (0, 10):
+
+            height = height * 0.9
+
+            z = z + 1
+
+            pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[width,length,height])
+
+
 
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,8 +1,13 @@
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
-p.loadSDF("box.sdf")
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+p.setGravity(0,0,-9.8) #gravity
+planeId = p.loadURDF("plane.urdf") #floor
+p.loadSDF("boxes.sdf")
 for i in range(0,6000):
     time.sleep(1/60)
     p.stepSimulation()
