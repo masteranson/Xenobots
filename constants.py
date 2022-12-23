9d187aab5898409f1b00748428ed3cf3705730f7
Anson Tsang <ytsang3@gatech.edu>
2022-12-23 20:44:02 +0800

    random weight search

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-numLoop = 6000
+numLoop = 2000
 motorMaxForce = 100
 frequency = 1
 phaseOffset = np.pi/4
---
+++ search.py
@@ -0,0 +1,5 @@
+import os
+
+for i in range(0,4):
+    os.system("python3 generate.py")
+    os.system("python3 simulate.py")
